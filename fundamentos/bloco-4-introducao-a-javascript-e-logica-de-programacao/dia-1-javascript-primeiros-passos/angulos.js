021e8961b10e522b5afd85bdff2994caeb62236e
Ismael Peccin <ismaelpeccin@gmail.com>
2021-07-28 19:32:16 -0300

    cria o arquivo angulos.js

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/angulos.js
@@ -0,0 +1,17 @@
+let a = -40; 
+
+let b = 40; 
+
+let c = 90; 
+
+let d = a + b +c; 
+
+if (d === 180) {
+    console.log(true)
+}
+else if (a < 0 || b < 0 || c < 0){
+    console.log("error")
+}
+else {
+    console.log(false)
+}
