9bb1d54ad2da9d874d923b5232b677a629bb05d4
Ismael Peccin <ismaelpeccin@gmail.com>
2021-07-29 14:23:16 -0300

    cria o arquivo exercicio7.js

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exrecicio7.js
@@ -0,0 +1,25 @@
+let notaPorcentagem = 101; 
+
+if (notaPorcentagem <0 || notaPorcentagem>100) { 
+  console.log("Erro") 
+}
+else if (notaPorcentagem >=90) {
+  console.log('A')
+}  
+else if (notaPorcentagem >=80) {   
+  console.log('B')
+}  
+else if (notaPorcentagem >=70) {
+  console.log('C')
+}  
+else if (notaPorcentagem >=60) {
+  console.log('D') 
+}   
+else if (notaPorcentagem >=50) {
+  console.log('E')
+}  
+else { 
+  console.log('F')
+}  
+
+
