d9a8f51d1bab66355f32fa947b1a1efeb5009f04
Ismael Peccin <ismaelpeccin@gmail.com>
2021-08-20 14:09:13 -0300

    html criado e array com estados ok no js

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,7 +1,7 @@
 const arrayTextoEstado = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
 
 function createStateOptions(array) {
-    for (let index = 0; index < arrayTextoEstado.length; index +=1) {
+    for (let index = 0; index < array.length; index +=1) {
         const stateSlect = document.getElementById('est');
         const stateOptions = document.createElement('option');
         stateSlect.appendChild(stateOptions);
@@ -9,3 +9,13 @@ function createStateOptions(array) {
     }
 }
 createStateOptions(arrayTextoEstado);
+
+function createDate() {
+    let data = document.getElementById('data');
+    let dataSplit = data.split('/');
+    for (let index = 0; index < dataSplit.length; index += 1) {
+    }
+}
+createDate(); 
+
+
