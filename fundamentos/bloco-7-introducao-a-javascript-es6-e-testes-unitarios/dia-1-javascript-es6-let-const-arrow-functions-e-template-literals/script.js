57f5f136d67c58c85a0b01c7d068f73f9a84dc91
Ismael Peccin <ismaelpeccin@gmail.com>
2021-08-27 18:53:40 -0300

    Parte I completa

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -28,3 +28,25 @@
   }
   testingScope(false);
 
+  //2.Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente. 
+  //Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+  //Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  // Construíndo uma função que ordena qualquer array: 
+  // function comparaNumeros(a,b) { 
+  //  if (a == b); 
+  //  return 0; 
+  //  if (a < b);
+  //  return -1;
+  //  if (a > b) 
+  //  return 1; 
+  // }
+  // O sort pode receber uma função de comparação que, dados dois valores, deve devolver um número inteiro. 
+  // É possível simplificar a função, dado que é possível substituir todos os 3 if por a-b. Pode-se usar também Arrows Functions, e então temos:
+  
+  oddsAndEvens.sort((a, b) => a - b);
+
+  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+
