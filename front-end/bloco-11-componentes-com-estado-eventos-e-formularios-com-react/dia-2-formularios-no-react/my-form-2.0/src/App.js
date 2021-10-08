704a8dd76854be7697377c71a3283b60d80d1a5e
Ismael Peccin <ismaelpeccin@gmail.com>
2021-10-08 20:09:45 -0300

    Cria os aqrquivos Form.js, PersonalForm.js, ProfessionalForm.js e altera o arquivo App.js

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return 
+const initialState = {
+  name: '',
+  email: '',
+  CPF: '',
+  Adress: '', 
+  city: '',
+
+};
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props) 
+    this.state = initialState; 
+  }
+
+
+  render() {
+    return
+  }
+
 }
 
 export default App;
