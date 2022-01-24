d3a2e1ab3250631e2ea23e85982e078eaedf05bb
Ismael Peccin <ismaelpeccin@gmail.com>
2022-01-24 18:39:00 -0300

    exercicios 10 a 15

--- back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/.sql
@@ -17,3 +17,19 @@ SELECT Name FROM Scientists ORDER BY Name;
 SELECT Name FROM Projects ORDER BY name DESC;
 
 SELECT CONCAT('O projeto', Name, 'precisou de', Hours, 'para ser concluído.') AS resultado FROM Projects;
+
+SELECT Name, Hours FROM Projects
+ORDER BY Hours DESC LIMIT 3;
+
+SELECT DISTINCT Project FROM AssignedTo;
+
+SELECT Name, Hours FROM Projects
+ORDER BY Hours DESC LIMIT 1;
+
+SELECT Name, Hours FROM Projects
+ORDER BY Hours LIMIT 1 OFFSET 1;
+
+SELECT * FROM Projects 
+ORDER BY Hours LIMIT 5;
+
+SELECT CONCAT('Existem', ' ',COUNT(Name),' ',  'cientistas na tabela Scientists.') FROM Scientists;
