9cbeb0998be43c7296ae8288bad2cf7de48d92a5
Ismael Peccin <ismaelpeccin@gmail.com>
2022-02-08 20:58:59 -0300

    Adciona input para nome e printa as informaçoes com console.log

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/index.js
@@ -1,7 +1,11 @@
 const imc =  require('./imc');
 const readline = require('readline-sync');
 
+
+const nome = readline.question('Qual seu nome? ');
 const peso = readline.questionFloat('Qual seu peso? ');
 const alt = readline.questionFloat('Qual sua altura? ');
+
+
 
 console.log('Seu IMC é:', imc(alt, peso));
