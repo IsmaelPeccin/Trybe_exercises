476089094243e08d315fa8c43b3ed0f3d1cbeae1
Ismael Peccin <ismaelpeccin@gmail.com>
2022-02-09 15:18:13 -0300

    Script para sortear numeros completo

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/sorteio.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/sorteio.js
@@ -1,2 +1,28 @@
-const generateNumbers = () => Math.floor(Math.random() * 10 + 1);
+const readline = require('readline-sync');
 
+const verificaNumero = (numero, resposta) => {
+  if(numero === resposta) {
+    return console.log('Parabéns, número correto!');
+  }
+  return console.log(`Opa, não foi dessa vez. O número era ${numero}`);
+} 
+
+const game = () => {
+
+  const numero  =  parseInt(Math.random() * 10);
+
+  const resposta = readline.questionInt('Digite um número entre 0 e 10: ');
+
+  verificaNumero(numero, resposta);
+
+  const novamente = readline.question(
+    'Deseja jogar novamente? (Digite s para sim, e qualquer outra coisa para não) '
+  );
+
+  if(novamente === 's'){
+    return game()
+  }
+  return console.log('OK, até a próxima!');
+}
+
+game();
