0ea4840a9140ebe585104fccd68314bfb7f5d7fe
Ismael Peccin <ismaelpeccin@gmail.com>
2022-02-09 16:16:53 -0300

    Iniciando exercicios do dia. Exercicio 1 parcialmente resolvido

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/Promises/index.js
@@ -0,0 +1,6 @@
+function p (param1, param2, param3) {
+  return new Promise((resolve, reject) => {
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number')
+    reject('Informe apenas números');
+  });
+}
