a344fe9de28cd4832352a48cab7147fd646dfa7e
Ismael Peccin <ismaelpeccin@gmail.com>
2022-02-09 16:29:26 -0300

    Promise do exercicio implementada

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/Promises/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/Promises/index.js
@@ -1,6 +1,14 @@
-function p (param1, param2, param3) {
+function isNumber (a, b, c) {
   return new Promise((resolve, reject) => {
-    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number')
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
     reject('Informe apenas números');
-  });
+    }
+   const result = ((a + b) * c );
+
+    if(result < 50 ) {
+      reject('Valor muito baixo');
+    }
+    resolve(result);
+    }
+  );
 }
