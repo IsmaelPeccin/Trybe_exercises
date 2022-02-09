d6d5c765eab55988b45c51148108cc98458f6d55
Ismael Peccin <ismaelpeccin@gmail.com>
2022-02-09 15:22:36 -0300

    Refatora codigo do arquivo index.js, que agora sera executado no arquivo imc.js, com o comando npm run imc

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -1,3 +1,13 @@
+const readline = require('readline-sync');
+const classImc = require('./classImc');
+
 const imc = (alt, peso) => ((peso)/Math.pow(alt,2)).toFixed(2);
 
-module.exports = imc;
+const nome = readline.question('Qual seu nome? ');
+const peso = readline.questionFloat('Qual seu peso? ');
+const alt = readline.questionFloat('Qual sua altura? ');
+
+const resultImc = imc(alt, peso);
+
+
+console.log(`Seu IMC é: ${resultImc}, ${classImc(resultImc)}`);
