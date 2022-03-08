0aedc217792692a0715ae51558f1abd48e4f043d
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-08 18:51:37 -0300

    Correçao migration books

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/migrations/20220308212629-create-books.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/migrations/20220308212629-create-books.js
@@ -23,10 +23,6 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
