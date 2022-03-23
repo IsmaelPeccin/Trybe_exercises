666a15444a1f77d763dee2de0a9e648a3b3623d0
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-23 17:30:41 -0300

    fix: refatora a função volume.ts

--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/typescript-exercises/volume.ts
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/typescript-exercises/volume.ts
@@ -1,15 +1,8 @@
-const volError = require('./error');
+import convertGlobal from "./convertGlobal";
 
-const volUnits = ["km³", "hm³", "dam³", "m³", "dm³", "cm³", "mm³"];
+const units = ["km³", "hm³", "dam³", "m³", "dm³", "cm³", "mm³"];
 
-function volumeConvert(value: number, forUnity: string, toUnity: string): number {
+function convert(value: number, forUnity: string, toUnity: string): number {
 
-  if (!volUnits.includes(forUnity)) volError(forUnity); 
-  if (!volUnits.includes(toUnity)) volError(toUnity); 
-
-  const forIndex = units.indexOf(forUnity); 
-  const toIndex = units.indexOf(toUnity); 
-  const exponent = (toIndex - forIndex);
-
-  return value * Math.pow(10, exponent);
+  return convertGlobal.convert(units, value, forUnity, toUnity);
 }
