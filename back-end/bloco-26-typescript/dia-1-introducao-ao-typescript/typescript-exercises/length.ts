b527054f1a1aba977e5ebd72702c3dc1aae87655
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-23 15:39:01 -0300

    feat: length.ts (ex1)

---
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/typescript-exercises/length.ts
@@ -0,0 +1,17 @@
+const units = ["km", "hm", "dam", "m", "dm", "cm", "mm"];
+
+function error(unity: string) {
+  throw new Error(`A unidade ${unity} não é uma unidade válida.`)
+}
+
+function convert(value: number, forUnity: string, toUnity: string): number {
+
+  if (!units.includes(forUnity)) error(forUnity); 
+  if (!units.includes(toUnity)) error(toUnity); 
+
+  const forIndex = units.indexOf(forUnity); 
+  const toIndex = units.indexOf(toUnity); 
+  const exponent = (toIndex - forIndex);
+
+  return value * Math.pow(10, exponent);
+}
