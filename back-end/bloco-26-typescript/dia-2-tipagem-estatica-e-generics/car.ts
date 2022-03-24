35593e3489125c7d7af2b8f6b90f04adde85eba5
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-24 19:06:24 -0300

    feat: implementando primeiro exercicio

---
+++ back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/car.ts
@@ -0,0 +1,68 @@
+enum Color {
+  BLACK = 'preto',
+  RED = 'vermelho',
+  WHITE = 'branco',
+  SILVER = 'prata',
+}
+
+enum Directions {
+  LEFT = "Esquerda",
+  RIGHT = "Direita",
+}
+
+enum Doors {
+  DRIVER = "porta do motorista",
+  RIDE = "porta do carona",
+  BEHIND_DRIVER = "porta atrás do motorista",
+  BEHIND_RIDE = "porta  atrás do carona",
+}
+
+
+class Car {
+  brand: string;
+  color: Color; 
+  doors: number; 
+  
+  constructor(brand: string, doors: number, color: Color) {
+      this.brand  = brand;
+      this.doors  = doors;
+      this.color  = color;
+  }
+
+  honk(): void {
+    console.log('Buzinando');
+  }
+
+  openTheDoor(door: Doors): void {
+    console.log(`Abrindo a  ${door}`);
+  } 
+
+  closeTheDoor(door: Doors): void {
+    console.log(`Fechando a  ${door}`);
+  }
+  
+  turnOff(): void {
+    console.log('Desligando')
+  }
+
+  turnOn(): void {
+    console.log('Ligando')
+  }
+
+  speedUp(): void {
+    console.log('Acelerando o carro')
+  }
+
+  speedDown(): void {
+    console.log('Reduzindo a velocidade do carro')
+  }
+
+  stop(): void {
+    console.log('Estacionando o carro')
+  }
+
+  turn(directions: Directions): void {
+    console.log(`Virando para a ${ directions }.`);
+  }
+
+}
