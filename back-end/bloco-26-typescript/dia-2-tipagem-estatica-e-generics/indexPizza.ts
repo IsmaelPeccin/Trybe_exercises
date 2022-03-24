67ab603b6e27fe22ba190212bbfc7f27bfa2dfe3
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-24 20:02:44 -0300

    feat: exercicio 4 completo

--- back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/indexPizza.ts
+++ back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/indexPizza.ts
@@ -1,18 +1,18 @@
-import Pizza from "./pizza";
+import { Pizza, PizzaComum, PizzaDoce, PizzaVegetariana } from "./pizza";
 
-const calabresa: Pizza = {
+const Calabresa: Pizza = {
     flavor: "Calabresa",
     slices: 8
 }
 
-console.log(calabresa);
+console.log(Calabresa);
 
-const marguerita: Pizza = {
+const Marguerita: Pizza = {
     flavor: "Marguerita",
     slices: 6
 }
 
-console.log(marguerita);
+console.log(Marguerita);
 
 
 const nutela: Pizza = {
@@ -21,3 +21,45 @@ const nutela: Pizza = {
 }
 
 console.log(nutela);
+
+const calabresa: PizzaComum = {
+    flavor: "Calabresa",
+    slices: 6
+}
+
+console.log(calabresa);
+
+const frango: PizzaComum = {
+    flavor: "Frango",
+    slices: 8
+}
+
+console.log(frango);
+
+const pepperoni: PizzaComum = {
+    flavor: "Pepperoni",
+    slices: 6
+}
+
+console.log(pepperoni);
+
+const marguerita: PizzaVegetariana = {
+    flavor: "Marguerita",
+    slices: 8
+}
+
+console.log(marguerita);
+
+const palmito: PizzaVegetariana = {
+    flavor: "Palmito",
+    slices: 8
+}
+
+console.log(palmito);
+
+const goiabadaEQueijo: PizzaDoce = {
+    flavor: "Goiabada com Queijo",
+    slices: 4
+}
+
+console.log(goiabadaEQueijo);
--- back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/pizza.ts
+++ back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/pizza.ts
@@ -1,8 +1,26 @@
 type Slices  = 4 | 6 | 8;
 
-interface IPizza {
+type Comum = "Calabresa" | "Frango" | "Pepperoni";
+type Vegetariana = "Marguerita" | "Palmito" | "Cogumelos";
+type Doce = "Nutela" | "Goiabada com Queijo" | "Marshmallow";
+
+
+export interface Pizza {
   flavor: string;
   slices: Slices;
 }
 
-export default IPizza;
+export interface PizzaComum extends Pizza {
+  flavor: Comum;
+  slices: Slices;
+}
+
+export interface PizzaVegetariana extends Pizza {
+  flavor: Vegetariana;
+  slices: Slices;
+}
+
+export interface PizzaDoce extends Pizza {
+  flavor: Doce;
+  slices: 4;
+}
