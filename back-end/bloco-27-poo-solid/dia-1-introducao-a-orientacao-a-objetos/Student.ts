59e0b996781b40b1ab483abce52bc9cce837c71b
Ismael Peccin <ismaelpeccin@gmail.com>
2022-04-04 21:23:09 -0300

    feat: Primeiro exercício

--- back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/Student.ts
+++ back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/Student.ts
@@ -12,5 +12,53 @@ class Student {
   }
 
   
+  public get enrollment() : string {
+    return this._enrollment;
+  }
 
+  
+  public set enrollment(v : string) {
+    this._enrollment = v;
+  }
+
+  
+  public get name() : string {
+    return this._name;
+  }
+  
+  
+  public set name(v : string) {
+    if (v.length < 3) {
+      throw new Error('O nome deve conter no mínimo 3 caracteres.');
+  }
+  this._name = v;
+  }
+
+  
+  public get examsGrade() : number[] {
+    return this._examsGrade;
+  }
+
+  
+  public set examsGrade(v : number[]) {
+    if (v.length > 4) {
+      throw new Error('A pessoa estudante só pode possuir 4 notas de provas.');
+  }
+    this._examsGrade = v;
+  }
+  
+  
+  public get worksGrade() : number[] {
+    return this._worksGrade;
+  }
+  
+  
+ 
+ public set worksGrade(v : number[]) {
+   if (v.length > 2) {
+     throw new Error('A pessoa estudante só pode possuir 2 notas de trabalho.');
+  }
+   this._worksGrade = v;
+ }
 }
+
